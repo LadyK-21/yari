b1a7d4f22b3c04ea5a4cc1b2e7cc2687215ffec7
Claas Augner <caugner@mozilla.com>
2022-05-19 17:46:14 +0200

    fix(typescript): add HydrationData type to ssr/render

--- ssr/render.ts
+++ ssr/render.ts
@@ -117,6 +117,16 @@ function* extractCSSURLs(css, filterFunction) {
   }
 }
 
+interface HydrationData {
+  doc?: any;
+  pageNotFound?: boolean;
+  hyData?: any;
+  pageTitle?: any;
+  possibleLocales?: any;
+  locale?: any;
+  noIndexing?: any;
+}
+
 export default function render(
   renderApp,
   {
@@ -127,7 +137,7 @@ export default function render(
     possibleLocales = null,
     locale = null,
     noIndexing = null,
-  } = {}
+  }: HydrationData = {}
 ) {
   const buildHtml = readBuildHTML();
   const webfontURLs = extractWebFontURLs();
@@ -146,7 +156,7 @@ export default function render(
 
   let pageDescription = "";
 
-  const hydrationData = {};
+  const hydrationData: HydrationData = {};
   if (pageNotFound) {
     pageTitle = `🤷🏽‍♀️ Page not found | ${pageTitle}`;
     hydrationData.pageNotFound = true;
@@ -265,7 +275,7 @@ export default function render(
   // Move the script tags from the body to the head.
   // That way the browser can notice, and start downloading these files sooner
   // but they'll still be executed after the first render.
-  $("body script[src]").appendTo("head");
+  $("body script[src]").appendTo($("head"));
   $("body script[src]").remove();
 
   return $.html();
